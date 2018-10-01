31af725af9929d7d11a8a4e786b551004f9b7519
20151074010034 <20151074010034@MC266446.ifrn.local>
2018-10-01 08:15:25 -0300

    Criando método da home

--- agendaspring/src/agendaspring/controller/HomeController.java
+++ agendaspring/src/agendaspring/controller/HomeController.java
@@ -1,6 +1,14 @@
 package agendaspring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class HomeController {
 
+	@RequestMapping("/")
+	public void home() {
+		System.out.println("Chamou o método da home!");
+	}
+	
 }
